9e7e6bf1ccd22887c1e7fe27b1999d5a0be7802d
darrel124568 <darrel124568@gmail.com>
2026-03-23 16:33:23 +0300

    made a login tracker

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+function createLoginTracker(userInfo) {
+  let attemptCount = 0
+  return (passwordAttempt) => {
+    attemptCount ++
+     if(attemptCount > 3) {
+      return "Account locked due to too many failed login attempts"
+    }
+    if(passwordAttempt === userInfo.password) {
+      return "Login successful"
+    }
+    else {
+      return `Attempt ${attemptCount}: Login failed`
+    }   
+  }  
+}
 
 
 
